11bdafdaf206a0c0a258b07d2dcabbd9a722c617
Jonathan Williams <tamlin@mac.com>
2018-09-30 18:36:21 -0400

    lint fixes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const url = require('url');
 
 
 const optionDefinitions = [
-    { name: 'url', alias: 'u', type: String }
+  { name: 'url', alias: 'u', type: String },
 ];
 
 const options = commandLineArgs(optionDefinitions);
@@ -15,62 +15,61 @@ const options = commandLineArgs(optionDefinitions);
 const myURL = new URL(options.url);
 const fbSetID = myURL.searchParams.get('set');
 
-if(null === fbSetID) {
-    console.log('No set ID found');
-    process.exit(1);
+if (null === fbSetID) {
+  console.log('No set ID found');
+  process.exit(1);
 }
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-    // Handle cookies.
-    if (fs.existsSync('./cookies')) {
-        console.log('Import saved cookies');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-        const cookies = JSON.parse(fs.readFileSync('./cookies', 'utf-8'));
-        await page.setCookie(...cookies);
-    }
+  // Handle cookies.
+  if (fs.existsSync('./cookies')) {
+    console.log('Import saved cookies');
 
-    // Visit page.
+    const cookies = JSON.parse(fs.readFileSync('./cookies', 'utf-8'));
+    await page.setCookie(...cookies);
+  }
+
+  // Visit page.
+  await page.goto(options.url, {
+    waitUntil: 'networkidle2',
+  });
+  await page.setViewport({ width: 1200, height: 1000 });
+
+  //  If login detected
+  if (await page.$('input[name=email]') !== null) {
+    console.log('Login form detected. Logging in...');
+
+    await page.type('input[name=email]', process.env.FB_EMAIL, { delay: 100 });
+    await page.type('input[name=pass]', process.env.FB_PASS, { delay: 100 });
+    await page.click('#u_0_2');
+    await page.waitForNavigation({
+      waitUntil: 'load',
+    });
+
+    // Go to the gallery page again, because once you log in it goes to your home page.
     await page.goto(options.url, {
-        waitUntil: 'networkidle2'
+      waitUntil: 'networkidle2',
     });
-    await page.setViewport({width: 1200, height: 1000});
 
-    //  If login detected
-    if (await page.$('input[name=email]') !== null) {
-        console.log('Login form detected. Logging in...');
-
-        await page.type('input[name=email]', process.env.FB_EMAIL, { delay: 100 });
-        await page.type('input[name=pass]', process.env.FB_PASS, { delay: 100 });
-        await page.click('#u_0_2');
-        await page.waitForNavigation({
-            waitUntil: 'load'
-        });
-
-        // Go to the gallery page again, because once you log in it goes to your home page.
-        await page.goto(options.url, {
-            waitUntil: 'networkidle2'
-        });
-
-        if (await page.$('input[name=email]') !== null) { // login failed
-            return Promise.reject('Error: login failed');
-        }
+    if (await page.$('input[name=email]') !== null) { // login failed
+      return Promise.reject('Error: login failed');
     }
+  }
 
-    console.log('Write cookies to file...');
-    fs.writeFileSync('./cookies', JSON.stringify(await page.cookies()), 'utf-8');
+  console.log('Write cookies to file...');
+  fs.writeFileSync('./cookies', JSON.stringify(await page.cookies()), 'utf-8');
 
-    await screenshotDOMElement(page, {
-        path: 'output/' + fbSetID + '.png',
-        selector: '#fbTimelinePhotosContent',
-        padding: 2
-    });
+  await screenshotDOMElement(page, {
+    path: 'output/' + fbSetID + '.png',
+    selector: '#fbTimelinePhotosContent',
+    padding: 2,
+  });
 
-    await browser.close();
+  await browser.close();
 })();
-
 
 
 /**
@@ -81,31 +80,33 @@ if(null === fbSetID) {
  * @return {!Promise<!Buffer>}
  */
 async function screenshotDOMElement(page, opts = {}) {
-    const padding = 'padding' in opts ? opts.padding : 0;
-    const path = 'path' in opts ? opts.path : null;
-    const selector = opts.selector;
+  const padding = 'padding' in opts ? opts.padding : 0;
+  const path = 'path' in opts ? opts.path : null;
+  const selector = opts.selector;
 
-    if (!selector)
-        throw Error('Please provide a selector.');
+  if (!selector) {
+    throw Error('Please provide a selector.');
+  }
 
-    const rect = await page.evaluate(selector => {
-        const element = document.querySelector(selector);
-        if (!element)
-            return null;
-        const { x, y, width, height } = element.getBoundingClientRect();
-        return { left: x, top: y, width, height, id: element.id };
-    }, selector);
 
-    if (!rect)
-        throw Error(`Could not find element that matches selector: ${selector}.`);
+  const rect = await page.evaluate(selector => {
+    const element = document.querySelector(selector);
+    if (!element)
+      return null;
+    const { x, y, width, height } = element.getBoundingClientRect();
+    return { left: x, top: y, width, height, id: element.id };
+  }, selector);
 
-    return await page.screenshot({
-        path,
-        clip: {
-            x: rect.left - padding,
-            y: rect.top - padding + 400,
-            width: rect.width + padding * 2,
-            height: rect.height + padding * 2
-        }
-    });
+  if (!rect)
+    throw Error(`Could not find element that matches selector: ${selector}.`);
+
+  return await page.screenshot({
+    path,
+    clip: {
+      x: rect.left - padding,
+      y: rect.top - padding + 400,
+      width: rect.width + padding * 2,
+      height: rect.height + padding * 2,
+    },
+  });
 }
